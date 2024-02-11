a09f0ac5e27a6c983c5f1ebabe23eb4583dfb473
Takogero <1_aim@bk.ru>
2024-02-11 17:03:13 +0300

    Initial Commit

---
+++ MyVideoPlayer/MyVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVideoPlayerApp.swift
+//  MyVideoPlayer
+//
+//  Created by MacBookAir on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
